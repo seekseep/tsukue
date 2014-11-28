29a6068d418461f914fecf4953de598dd2d203ca
mkn <referlol21@gmail.com>
2014-11-28 13:51:32 +0900

    しのりょー用にindex.phpでtopページを確認できるように
    コミット名通り
    元ファイルはindex2.php

---
+++ public/common/php/_add.php
@@ -0,0 +1,6 @@
+<div>
+	<div class="form-group">
+		<label for="package">パッケージ名</label>
+	</div>
+
+</div>
--- public/create/index2.php
+++ public/create/index2.php
@@ -1,5 +1,4 @@
 <?php include_once '../common/php/_head.php'; ?>
 <?php include_once '../common/php/_header.php'; ?>
-<?php include_once '../common/php/_top.php'; ?>
 <?php include_once '../common/php/_footer.php'; ?>
 <?php include_once '../common/php/_foot.php'; ?>
