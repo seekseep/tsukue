7f0c65227f1e05afee4b8811df992784b4204b7f
mkn <referlol21@gmail.com>
2014-11-28 16:29:12 +0900

    cssファイル名の微修正

--- public/common/php/_dashboard.php
+++ public/common/php/_dashboard.php
@@ -1,7 +1,7 @@
 <div id="main" class="container">
 	<div class="row" >
 		<div class="col-md-3 col-md-offset-9 content_header">
-			<a class="add-btn">パッケージの追加</a>
+			<a class="add_btn">パッケージの追加</a>
 		</div>
 	</div>
 	<div class="package col-md-offset-1 col-md-10 col-md-offset-1 row" >	<!-- パッケージ全体 -->
@@ -15,8 +15,10 @@
 					<a class="package_name">パッケージ名</a>
 				</div>
 				<div class="col-md-1"></div>
-				<div class="col-md-5 package_info"><a>2014年11月20日</a></div>
-				<div class="col-md-1"></div>
+				<div class="col-md-3 package_info"><a>2014年11月20日</a></div>
+				<div class="col-md-3">
+					<a class="edit_btn">パッケージ編集</a>
+				</div>
 			</div>
 			<div class="tags col-md-8">
 				<ul >
@@ -33,32 +35,4 @@
 		</p>
 		</div>
 	</div>
-	<div class="package col-md-offset-1 col-md-10 col-md-offset-1 row" >	<!-- パッケージ全体 -->
-		<div class="col-md-3 package_icon">			<!-- Package画像 -->
-			<a><img src="../common/image/numakuro.png" alt="#"></a>
-		</div>
-		<div class="col-md-9 row"> 					<!-- Package右側 -->
-			<div class="row">
-				<div class="col-md-1"></div>
-				<div class="col-md-4">
-					<a class="package_name">パッケージ名</a>
-				</div>
-				<div class="col-md-1"></div>
-				<div class="col-md-5 package_info"><a>2014年11月20日</a></div>
-				<div class="col-md-1"></div>
-			</div>
-			<div class="tags col-md-8">
-				<ul>
-					<li>Tag1</li>
-					<li>Tag2</li>
-					<li>Tag3</li>
-				</ul>
-			</div>
-			<div class="links">
-				<img src="../common/image/Twitter_icon.png">
-				<img src="../common/image/QR_icon.png">
-			</div>
-			<p class="package_detail text-info">ここにパッケージの説明が表示されます。</p>
-		</div>
-	</div>
-</div>1
+</div>
--- public/common/php/_head.php
+++ public/common/php/_head.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="../common/css/dashboard.css">
+	<link rel="stylesheet" href="../common/css/creator.css">
 	<link rel="stylesheet" href="../common/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
   	<script src="../common/js/bootstrap.min.js"></script>
