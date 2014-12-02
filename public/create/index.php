6d301884cff698ca3ce481082eec0e5bb8a12ec3
mkn <referlol21@gmail.com>
2014-12-02 13:47:19 +0900

    topさんぷる？

--- public/create/index.php
+++ public/create/index.php
@@ -1,9 +1,30 @@
 <?php include_once '../common/php/_head.php'; ?>
+
 <?php include_once '../common/php/_create_header.php'; ?>
+<div class="container">
 
+	<div id="top_header_1" class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h1 class="text-center">Virtual Table Platform</h1>
+		</div>
+	</div>
 
+	<div id="top_header_2" class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h1 class="text-center">
+				<button type="button" class="btn btn-primary btn-lg "> パッケージを投稿する</button>
+			</h1>
+		</div>
+	</div>
+
+	<div id="top_header_3" class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h1 class="text-center">
+				<button type="button" class="btn btn-primary btn-lg ">play画面に移動</button>
+			</h1>
+		</div>
+	</div>
+
+</div>
 <?php include_once '../common/php/_create_footer.php'; ?>
-<!-- SCRIPT -->
-<script></script>
-<!-- SCRIPT -->
 <?php include_once '../common/php/_foot.php'; ?>
