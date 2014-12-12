30cb4e40053f6b7f130fc0405f907c65bf667a8c
refer21 <referlol21@gmail.com>
2014-12-12 16:56:01 +0900

    パッケージに画像を登録
    add.phpで画像が登録出来るようになった
    creator.cssが2個あったので、旧のものを削除

--- public/create/add.php
+++ public/create/add.php
@@ -1,16 +1,17 @@
 <?php include_once 'common/php/_head.php'; ?>
 <?php include_once 'common/php/_header.php'; ?>
-<script type="text/javascript">			//http://www.yoheim.net/blog.php?q=20120332
+<!-- <script type="text/javascript">			//http://www.yoheim.net/blog.php?q=20120332
 	function preview (e) {				
-  if (!e.files.length) return;
+		if (!e.files.length) return;
   // ファイルを1件ずつ処理する
   var errMsg = "";
   for (var i = 0; i < e.files.length; i++) {
-    var file = e.files[i];
+  	var file = e.files[i];
     // 想定したMIMEタイプでない場合には処理しない
     if (!/^image\/(png|jpeg|gif)$/.test(file.type)) {
-      errMsg += "ファイル名: " + file.name + ", 実際のMIMEタイプ: " + file.type + "\n\n";
-      continue;
+    	errMsg = "対応していないファイルです。\n"
+    	+ "png,jpeg,bmp形式でアップロードしてください。\n";
+    	continue;
     }
     // Imageを作成
     // imgとかfr変数は、ループごとに上書きされるので、
@@ -21,68 +22,91 @@
     var fr = new FileReader();
     fr.tmpImg = img;
     fr.onload = function () {
-      this.tmpImg.src = this.result;
-      this.tmpImg.onload = function () {
-        document.getElementById('previewArea').appendChild(this);
-      }
+    	this.tmpImg.src = this.result;
+    	this.tmpImg.onload = function () {
+    		document.getElementById('previewArea').appendChild(this);
+    	}
     }
     // 画像読み込み
     fr.readAsDataURL(file);
-  }
+}
 
   // エラーがあれば表示する
   if (errMsg != "") {
-    errMsg = "以下ファイルはMIMEタイプが対応していません。\n"
-      + "MIMEタイプはimage/png, image/jpeg, image/gifのみ対応です。\n\n"
-      + errMsg;
-    alert(errMsg);
+  	alert(errMsg);
   }
 }							//ここまでソース丸コピ
-</script>				
+</script>		 -->
+<script>
+	window.addEventListener("load", function(){
+		var btn = document.querySelector("#addFileInputButton");
+		var frm = document.querySelector("#addPackageForm");
+		var idx = 1;
+
+		btn.addEventListener("click", function(){
+			addInputFile(1)
+		})
+
+		function addInputFile(num){
+			if(num === NaN){
+				num = 1
+			}
+			for(var i = 0; i < num; i++){
+				var fileInputWrap = document.createElement("div")
+				fileInputWrap.innerHTML = '<p><label>表[' + idx +']<input name="front" type="file"></label></p><p><label>裏[' + idx + ']<input name="back" type="file"></label></p>'
+				idx++;
+				frm.appendChild(fileInputWrap);	
+			}
+		}
+
+		addInputFile(5)
+	})
+</script>
+
 <div id="main" class="container">
-	<form class="form-horizontal" role="form">
-		<div class="col-md-9">			<!-- パッケージ左全体 -->	
-			<div class="form-group">	<!-- パッケージ名 -->
+	<form class="form-horizontal" role="form" action="add.php" method="get">
+		<div class="col-md-offset-1 col-md-9">				<!-- パッケージ左全体 -->	
+			<div class="form-group">		<!-- パッケージ名 -->
 				<label for="package_name" class="col-md-3 control-label">パッケージ名</label>
-				<div class="col-md-6">
+				<div class="col-md-9">
 					<input type="text" class="form-control" id="package_name" placeholder="パッケージ名を入力してください">
 				</div>
 			</div>	
-			<div class="form-group">	<!-- パッケージ詳細 -->
+			<div class="form-group">		<!-- パッケージ詳細 -->
 				<label for="package_info" class="col-md-3 control-label">パッケージ詳細</label>
-				<div class="col-md-6">
+				<div class="col-md-9">
 					<textarea name="package_info" rows="3" cols="65" placeholder="パッケージの詳細を入力してください"></textarea>
 				</div>
 			</div>
-			<div class="form-group">	<!-- タグ -->
+			<div class="form-group">		<!-- タグ -->
 				<label for="package_tag" class="col-md-3 control-label">検索タグ名</label>
-				<div class="col-md-6">
+				<div class="col-md-9">
 					<input type="text" class="col-md-2 form-control" id="package_tag">
 				</div>
 			</div>
-			<div class="row">			<!-- 場の背景 -->
-				<label for="package_name" class="col-md-3 control-label">場の背景</label>
-				<div class="col-md-6">
-					<input type="file" class="form-control" accept="image/*" name="" value="">
-				</div>
-			</div>
-			<div class="row">			<!-- 手札の背景 -->
-				<label for="package_name" class="col-md-3 control-label">手札の背景</label>
-				<div class="col-md-6">
+			<div class="form-group">		<!-- パッケージイメージ -->
+				<label for="package_img" class="col-md-3 control-label">パッケージイメージ</label>
+				<div class="col-md-9">
 					<input type="file" class="form-control"　accept="image/*" name="" value="">
 				</div>
 			</div>
-		</div>
-		<div class="col-md-3">		<!-- パッケージ画像 -->
-			<div class="form-group">
-				<img src="common/image/numakuro.png" class="img-responsive" alt="Responsive image"height="440"width="452">
+			<div class="form-group">		<!-- 場の背景 -->
+				<label for="package_fieldbg" class="col-md-3 control-label">場の背景</label>
+				<div class="col-md-9">
+					<input type="file" class="form-control" accept="image/*" name="" value="">
+				</div>
 			</div>
-		</div>
-		<div>		 					<!-- ファイル投稿部 -->
-			<div id="previewArea" class="col-md-offset-1 col-md-6"style=" height:150px; background-color:#fff;"></div>
-			<div class="col-md-4">					
-				<input type="file" name="pic_upload" onchange="preview(this);"/>
+			<div class="form-group">		<!-- 手札の背景 -->
+				<label for="package_handbg" class="col-md-3 control-label">手札の背景</label>
+				<div class="col-md-9">
+					<input type="file" class="form-control"　accept="image/*" name="" value="">
+				</div>
 			</div>
+			<div class="form-group">		<!-- ファイル投稿部 -->
+				<input id="addFileInputButton" type="button" value="カード追加">	
+				<div id="addPackageForm"></div>
+			</div>
+
 		</div>
 		<div class="col-md-offset-11 col-md-1">	
 			<div class="form-group">
--- public/create/dashboard.php
+++ public/create/dashboard.php
@@ -1,31 +1,30 @@
 <?php include_once 'common/php/_head.php'; ?>
 <?php include_once 'common/php/_header.php'; ?>
-<div id="main" class="container">
+<div id="main" class="container" style="min-height:600px">
 	<div class="row">
 		<div class="col-md-3 col-md-offset-9 content_header">
 			<a class="add_btn" href="add.php">パッケージの追加</a>
 		</div>
 	</div>
-	<div class="package col-md-offset-1 col-md-10 row" >	<!-- パッケージ全体 -->
+	<div class="package col-md-offset-1 col-md-10 row">	<!-- パッケージ全体 -->
 		<div class="col-md-3 package_icon">			<!-- Package画像 -->
 			<a><img src="common/image/numakuro.png" alt="#"></a>
 		</div>
 		<div class="col-md-9 row"> 					<!-- Package右側 -->
 			<div class="row">
-				<div class="col-md-offset-1 col-md-3 package_name">
-					<a>パッケージ名</a>
+				<div class="col-md-offset-1 col-md-3 package_name text-danger">
+					パッケージ名
 				</div> 
-				<div class="col-md-offset-1 col-md-3 package_info"><a>2014年11月20日</a></div>
+				<div class="col-md-offset-1 col-md-3 package_info">
+					2014年11月20日</div>
 				<div class="col-md-offset-1 col-md-3">
 					<a class="edit_btn">パッケージ編集</a>
 				</div>
 			</div>
 			<div class="tags col-md-8">
-				<ul >
-					<li>Tag1</li>
-					<li>Tag2</li>
-					<li>Tag3</li>
-				</ul>
+			<span class="label label-success">
+				tag2
+			</span>
 			</div>
 			<div class="links">
 				<a href="http://twitter.com"><img src="common/image/Twitter_icon.png"></a>
