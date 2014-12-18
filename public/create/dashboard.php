c19525185dec81dfe4f7830f424fbb071b20cea5
refer21 <referlol21@gmail.com>
2014-12-19 05:17:30 +0900

    Dashboardまとめた
    コミット忘れてた

--- public/create/dashboard.php
+++ public/create/dashboard.php
@@ -6,56 +6,75 @@
 			<a class="btn btn-info btn-lg" href="add.php">パッケージの追加</a>
 		</div>
 	</div>
-	<div class="package col-md-offset-1 col-md-10 row">	<!-- パッケージ全体 -->
-		<div class="col-md-3 package_icon">			<!-- Package画像 -->
-			<img src="common/image/numakuro.png" class="img-thumbnail" alt="#">
-		</div>
-		<div class="col-md-9 row"> 					<!-- Package右側 -->
-			<div class="row">
-				<div class="col-md-offset-1 col-md-3 package_name text-danger">
-					パッケージ名
-				</div> 
-				<div class="col-md-offset-1 col-md-3 package_info">
-					2014年11月20日</div>
-					<div class="col-md-offset-1 col-md-3">
-						<a class="btn btn-primary">パッケージ編集</a>
-					</div>
-				</div>
-				<div class="tags col-md-8">
-					<span class="label label-success">tag1</span>
-					<span class="label label-success">tag1</span>
-					<span class="label label-success">tag1</span>
-					<span class="label label-success">tag1</span>
-					<span class="label label-success">tag1</span>
-					
-				</div>
-				<div class="links">
-					<a href="http://twitter.com"><img src="common/image/Twitter_icon.png"></a>
+<?php
 
-					<a data-toggle="modal" data-target="#myModal"><img src="common/image/QR_icon.png"></a>
-				</div>
-				<!-- Modal -->
-				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-								<h4 class="modal-title" id="myModalLabel">Modal title</h4>
-							</div>
-							<div class="modal-body">
-								<img src="http://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=http://kinako.asia/" alt=""> 
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- Modal -->
-				<p class="package_detail text-info">ここにパッケージの説明が表示されます。
-				</p>
+
+// $results =　array(
+// 	"id" => "p_00001",
+// 	"name" => "パッケージ1",
+// 	"author" => "大谷",
+// 	"time" => "2025/12/31",
+// 	"tag" => array("トランプ","シンプル","ゲーム","オリジナル","オリジナル画像","ヌマクロー","ポケモン",),
+// 	"description" => "このパッケージは大谷の熱いゲームへの思いがたくさん込められています。スマートフォンの枠に収まるようにがんばりました。",
+// 	"image" => "numakuro.png",
+// 	"hand" => "numakuro.png",
+// 	"field" => "numakuro.png",
+// 	),
+
+// $package_id = "";
+$package_name = "ヌマクロートランプ";
+$package_description = "ここにパッケージの内容が表示されるはずです";
+$package_tag = array("トランプ","シンプル","ゲーム","オリジナル","オリジナル画像","ヌマクロー","ポケモン")	;
+$package_image = "common/image/numakuro.png"; 
+$package_time = "1234/56/78 12:34:56"
+
+?>
+
+<div class="package col-md-offset-1 col-md-10 row">	<!-- パッケージ全体 -->
+	<div class="col-md-3 package_icon">			<!-- Package画像 -->
+		<img src=" <?php echo $package_image ?> " class="img-thumbnail" alt="#" >
+	</div>
+	<div class="col-md-9 row"> 					<!-- Package右側 -->
+		<div class="row">
+			<div class="col-md-offset-1 col-md-4 package_name text-danger">
+			<?php echo $package_name; ?>
+			</div> 
+			<div class="col-md-4 package_time">
+				<?php echo $package_time ?>
+			</div>
+			<div class="col-md-3">
+				<a class="btn btn-primary">パッケージ編集</a>
 			</div>
 		</div>
+		<div class="tags col-md-8">
+			<?php 
+			foreach ($package_tag as $value) {
+		echo ('<span class="label label-success">'. $value .'</span>');
+			}
+			?>
+		</div>
+		<div class="links">
+			<a href="http://twitter.com"><img src="common/image/Twitter_icon.png"></a>
+
+			<a data-toggle="modal" data-target="#myModal"><img src="common/image/QR_icon.png"></a>
+		</div>
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">	<!-- Modal -->
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-body">
+						<img src="http://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=http://kinako.asia/" alt=""> 
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div><!-- Modal -->
+		<p class="package_description text-info">
+			<?php echo($package_description) ?>
+		</p>
 	</div>
-	<?php include_once 'common/php/_footer.php'; ?>
-	<?php include_once 'common/php/_foot.php'; ?>
+</div>
+</div>
+<?php include_once 'common/php/_footer.php'; ?>
+<?php include_once 'common/php/_foot.php'; ?>
